87b2f5aa48817f7d45a2d4447ee5f231d58427d5
JakubKivi <jakub.kivi.kuziemski@gmail.com>
2021-05-28 00:49:05 +0200

    link to modal works properly

--- js/closingModal.js
+++ js/closingModal.js
@@ -1,20 +1,45 @@
 var closedModalHashStateId = "#main";
 var openModalHashStateId = "#window";
 
+//window.location.hash = closedModalHashStateId;
+
 var FPS = 5;
 
 /* Updating the hash state creates a new entry
  * in the web browser's history. The latest entry in the web browser's
  * history is "modal.html#modalClosed". */
-if(window.location.hash != "#contact")window.location.hash = closedModalHashStateId;
+if(window.location.hash != "#contact"
+    &&window.location.hash != "#taflModal"
+    &&window.location.hash != "#araModal"
+    &&window.location.hash != "#calkModal"
+    &&window.location.hash != "#lampModal"
+    &&window.location.hash != "#wyswModal"
+    &&window.location.hash != "#jackModal"
+    &&window.location.hash != "#zasiModal"
+    &&window.location.hash != "#grzesModal")
+        window.location.hash = closedModalHashStateId;
 
 
-var chosenModal = document.getElementById('mod');
+document.querySelectorAll('.portfolio-item').forEach(item => {
+  item.addEventListener('click', event => {
+    console.log('loool');
+ 	window.location.hash = openModalHashStateId;
+ 	toCloseModal();
+  })
+})
 
-chosenModal.onclick = function() {
-	console.log('loool');
-	window.location.hash = openModalHashStateId;
-	toCloseModal();
+document.addEventListener("keydown", ({key}) => {
+    if (key === "Escape"&&window.location.hash == openModalHashStateId){
+        window.location.hash = closedModalHashStateId;
+    }
+})
+
+function handleBackPress(event) {
+    console.log("dupa");
+    event.preventDefault();
+    event.stopPropagation();
+    $('.modal-dialog').modal('hide');
+    $('.modal-backdrop').remove();
 }
 
 function toCloseModal(){
@@ -23,6 +48,6 @@ function toCloseModal(){
     }, 1000 / FPS);
 
 	if(window.location.hash==closedModalHashStateId){
-		//document.getElementsByTagName("mod")[0].setAttribute("aria-hidden", "true"); 
+		$('.modal').modal('hide');
 	}
 }
