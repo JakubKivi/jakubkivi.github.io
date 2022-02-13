bf6537835504906bff0331ad3db3f1f60768e3d5
jakubkivi <jakub.kivi.kuziemski@gmail.com>
2022-02-13 23:27:32 +0100

    start page almost done

---
+++ jakubkivi2.0/js/animations.js
@@ -0,0 +1,90 @@
+
+const ab1 = KUTE.fromTo(
+  '#a1',
+  { path: '#a1' },
+  { path: '#b1' },    
+  { repeat: 9999, duration: 8000, yoyo: true }
+);
+const ab3 = KUTE.fromTo(
+  '#a2',
+  { path: '#a2' },
+  { path: '#b2' },    
+  { repeat: 9999, duration: 4000, yoyo: true }
+);
+const ab4 = KUTE.fromTo(
+  '#a3',
+  { path: '#a3' },
+  { path: '#b3' },    
+  { repeat: 9999, duration: 7000, yoyo: true }
+);
+const ab5 = KUTE.fromTo(
+  '#a4',
+  { path: '#a4' },
+  { path: '#b4' },    
+  { repeat: 9999, duration: 4500, yoyo: true }
+);
+const ab6 = KUTE.fromTo(
+  '#a5',
+  { path: '#a5' },
+  { path: '#b5' },    
+  { repeat: 9999, duration: 5800, yoyo: true }
+);
+const ab7 = KUTE.fromTo(
+  '#a6',
+  { path: '#a6' },
+  { path: '#b6' },    
+  { repeat: 9999, duration: 5000, yoyo: true }
+);
+const ab8 = KUTE.fromTo(
+  '#a7',
+  { path: '#a7' },
+  { path: '#b7' },    
+  { repeat: 9999, duration: 7800, yoyo: true }
+);
+const ab9 = KUTE.fromTo(
+  '#a8',
+  { path: '#a8' },
+  { path: '#b8' },    
+  { repeat: 9999, duration: 8100, yoyo: true }
+);
+const ab10 = KUTE.fromTo(
+  '#a9',
+  { path: '#a9' },
+  { path: '#b9' },    
+  { repeat: 9999, duration: 7900, yoyo: true }
+);
+const ab11 = KUTE.fromTo(
+  '#a10',
+  { path: '#a10' },
+  { path: '#b10' },    
+  { repeat: 9999, duration: 8000, yoyo: true }
+);
+
+const ab2 = KUTE.fromTo(
+  '#a11',
+  { path: '#a11' },
+  { path: '#b11' },    
+  { repeat: 9999, duration: 6000, yoyo: true }
+);
+
+const n = KUTE.fromTo(
+  '#n1',
+  { path: '#n1' },
+  { path: '#n2' },    
+  { repeat: 9999, duration: 4000, yoyo: true }
+);
+
+window.onload = function() {
+  ab1.start();
+	ab2.start();
+	ab3.start();
+	ab4.start();
+	ab5.start();
+	ab6.start();
+	ab7.start();
+	ab8.start();
+	ab9.start();
+	ab10.start();
+	ab11.start();
+  n.start();
+};
--- jakubkivi2.0/js/script.js
+++ jakubkivi2.0/js/script.js
@@ -4,7 +4,6 @@ var wave = document.getElementById("wave-navbar-collapse");
 var fold = false;
 
 btn.onclick =function(){
-	console.log('dupa');
 	if(!fold){
 		wave.classList.add("wave-navbar-collpase-unfold");
 		fold=true;
@@ -15,93 +14,60 @@ btn.onclick =function(){
 	}
 }
 
+function hover(element)
+{
+  var activeNav = document.getElementsByClassName("navbar-selected-custom").item(0);
+  activeNav.classList.add("helper-custom");
+  activeNav.classList.remove("navbar-selected-custom");
+  element.classList.add("navbar-selected-custom");
 
-const ab1 = KUTE.fromTo(
-  '#a1',
-  { path: '#a1' },
-  { path: '#b1' },    
-  { repeat: 9999, duration: 8000, yoyo: true }
-);
-const ab3 = KUTE.fromTo(
-  '#a2',
-  { path: '#a2' },
-  { path: '#b2' },    
-  { repeat: 9999, duration: 4000, yoyo: true }
-);
-const ab4 = KUTE.fromTo(
-  '#a3',
-  { path: '#a3' },
-  { path: '#b3' },    
-  { repeat: 9999, duration: 7000, yoyo: true }
-);
-const ab5 = KUTE.fromTo(
-  '#a4',
-  { path: '#a4' },
-  { path: '#b4' },    
-  { repeat: 9999, duration: 4500, yoyo: true }
-);
-const ab6 = KUTE.fromTo(
-  '#a5',
-  { path: '#a5' },
-  { path: '#b5' },    
-  { repeat: 9999, duration: 5800, yoyo: true }
-);
-const ab7 = KUTE.fromTo(
-  '#a6',
-  { path: '#a6' },
-  { path: '#b6' },    
-  { repeat: 9999, duration: 5000, yoyo: true }
-);
-const ab8 = KUTE.fromTo(
-  '#a7',
-  { path: '#a7' },
-  { path: '#b7' },    
-  { repeat: 9999, duration: 7800, yoyo: true }
-);
-const ab9 = KUTE.fromTo(
-  '#a8',
-  { path: '#a8' },
-  { path: '#b8' },    
-  { repeat: 9999, duration: 8100, yoyo: true }
-);
-const ab10 = KUTE.fromTo(
-  '#a9',
-  { path: '#a9' },
-  { path: '#b9' },    
-  { repeat: 9999, duration: 7900, yoyo: true }
-);
-const ab11 = KUTE.fromTo(
-  '#a10',
-  { path: '#a10' },
-  { path: '#b10' },    
-  { repeat: 9999, duration: 8000, yoyo: true }
-);
+}
 
-const ab2 = KUTE.fromTo(
-  '#a11',
-  { path: '#a11' },
-  { path: '#b11' },    
-  { repeat: 9999, duration: 6000, yoyo: true }
-);
+function hoverOff(element)
+{
+  var activeNav = document.getElementsByClassName("helper-custom").item(0);
+  activeNav.classList.add("navbar-selected-custom");
+  activeNav.classList.remove("helper-custom");
+  if(activeNav!=element)
+    element.classList.remove("navbar-selected-custom");
+}
 
-const n = KUTE.fromTo(
-  '#n1',
-  { path: '#n1' },
-  { path: '#n2' },    
-  { repeat: 9999, duration: 4000, yoyo: true }
-);
 
-window.onload = function() {
-  ab1.start();
-	ab2.start();
-	ab3.start();
-	ab4.start();
-	ab5.start();
-	ab6.start();
-	ab7.start();
-	ab8.start();
-	ab9.start();
-	ab10.start();
-	ab11.start();
-  n.start();
-};
+$(document).ready(function(){
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  $("#navbarNav a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top-120
+      }, 600, function(){
+   
+        window.location.hash = hash;
+        // var activeNav = document.getElementsByClassName("navbar-selected-custom").item(0);
+        // activeNav.classList.remove("navbar-selected-custom");
+        // this.parentElement.classList.add("navbar-selected-custom");
+      });
+    }
+  });
+
+  $("#headerArrow a").on('click', function(event) {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top-120
+      }, 600, function(){
+   
+        window.location.hash = hash;
+      });
+    
+  });
+
+
+});
+
